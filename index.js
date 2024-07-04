17af2598d501d30751110daf15be6607c4e969c6
Ajay <kereajay578@gmail.com>
2024-07-04 12:20:43 +0530

    adding all the files for last time

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const Rimagerouter=require('./Rimage.js');
 const app=express();
 app.use(Rimagerouter);
 app.get('/',(req,res)=>{
-    res.json({status:'success',date:new Date(),message:'Welcome to Random Image Generator API',forRandomimage:"https://noderandomimageapi-1.onrender.com/Rimage"})
+    res.json({status:'success',date:new Date(),message:'Welcome to Random Image Generator API',forRandomimage:"use /Rimage"})
     
 })
 app.listen(7894,()=>console.log("server running on port 7894"));
